b13a2d5af157fd999d3d4d319118ea993aa6f568
chenyike <chenyike.upenn@gmail.com>
2015-04-13 15:01:11 -0400

    April 13

--- src/battleship/Ocean.java
+++ src/battleship/Ocean.java
@@ -26,85 +26,94 @@ public class Ocean {
 		this.shipsSunk = 0;
 	}
 
-	void placeAllShipsRandomly(){
-		boolean horizontal;
+	int[] generateRandom() {
+		int[] RandomVariable = new int[3]; 
 		int Row = new Random().nextInt(10);
 		int Column = new Random().nextInt(10);
-		if(new Random().nextInt(2)==1){
-			horizontal=true;
+		int horizontal = new Random().nextInt(2);
+		RandomVariable[1]= Row;
+		RandomVariable[2]=Column;
+		RandomVariable[3]=horizontal;
+		return RandomVariable;
+	}
+
+	void placeAllShipsRandomly(){
+		boolean horizontal;
+		if (generateRandom()[2] == 1){
+			 horizontal= true;
 		}
 		else{
-			horizontal=false;
+			horizontal = false;
 		}
-		battleship.placeShipAt(Row,Column, horizontal, ocean);	
+		battleship.placeShipAt(generateRandom()[0],generateRandom()[1], horizontal, ocean);	
 	}
-	
+
 	/**
-     * Returns true if the given location contains a ship, false if it does not.
-     * @param row
-     * @param column
-     * @return
-     */
-    public boolean isOccupied(int row, int column){
-            
-        return true;
-    }
-    
-    
-    /**
-     * Returns true if the given location contains a ”real” ship, still afloat, (not an EmptySea), false if it does not. 
-     * @param row
-     * @param column
-     * @return
-     */
-    public boolean shootAt(int row, int column){
-        return true;
-    }
-    
-    /**
-     * Returns the number of shots fired (in this game).
-     * @return
-     */
-    public  int getShotsFired(){
-        return 1;
-    }
-    
-    /**
-     * Returns the number of hits recorded (in this game). All hits are counted
-     * @return
-     */
-    public int getHitCount(){
-        return 1;
-    }
-    
-    /**
-     * Returns the number of ships sunk (in this game).
-     * @return
-     */
-    public int getShipsSunk(){
-        return 1;
-    }
-    
-    /**
-     * Returns true if all ships have been sunk, otherwise false.
-     * @return
-     */
-    public boolean isGameOver(){
-        return true;
-    }
-    
-    /**
-     * Returns the 10x10 array of ships.
-     * @return
-     */
-    public Ship[][] getShipArray(){
-        return null;
-    }
-    
-    /**
-     * Prints the ocean. 
-     */
-    public void print(){
-    }
+	 * Returns true if the given location contains a ship, false if it does not.
+	 * @param row
+	 * @param column
+	 * @return
+	 */
+	public boolean isOccupied(int row, int column){
+
+		return true;
+	}
+
+
+	/**
+	 * Returns true if the given location contains a ”real” ship, still afloat, (not an EmptySea), false if it does not. 
+	 * @param row
+	 * @param column
+	 * @return
+	 */
+	public boolean shootAt(int row, int column){
+		return true;
+	}
+
+	/**
+	 * Returns the number of shots fired (in this game).
+	 * @return
+	 */
+	public  int getShotsFired(){
+		return 1;
+	}
+
+	/**
+	 * Returns the number of hits recorded (in this game). All hits are counted
+	 * @return
+	 */
+	public int getHitCount(){
+		return 1;
+	}
+
+	/**
+	 * Returns the number of ships sunk (in this game).
+	 * @return
+	 */
+	public int getShipsSunk(){
+		return 1;
+	}
+
+	/**
+	 * Returns true if all ships have been sunk, otherwise false.
+	 * @return
+	 */
+	public boolean isGameOver(){
+		return true;
+	}
+
+	/**
+	 * Returns the 10x10 array of ships.
+	 * @return
+	 */
+	public Ship[][] getShipArray(){
+		return null;
+	}
+
+	/**
+	 * Prints the ocean. 
+	 */
+	public void print(){
+	}
 
 }
